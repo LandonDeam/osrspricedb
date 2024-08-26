4d7f41ba9b7ba3dc0aeaddd10b75d5907220c34e
Landon Deam <landondeam@gmail.com>
2024-08-26 19:27:08 -0400

    got rid of using namespace

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,25 @@
 #include "./main.h"
 
-using namespace Poco;
-using namespace Net;
-
 int main() {
   try {
     // Initializing variables
-    const URI uri("https://prices.runescape.wiki");
-    initializeSSL();
+    const Poco::URI uri("https://prices.runescape.wiki");
+    Poco::Net::initializeSSL();
     // TODO - Need to make an actual certificate handler :P
-    SharedPtr<InvalidCertificateHandler> ptrHandler =
-      new AcceptCertificateHandler(false);
-    Context::Ptr ptrContext = new Context(Context::CLIENT_USE, "");
-    SSLManager::instance().initializeClient(0, ptrHandler, ptrContext);
+    Poco::SharedPtr<Poco::Net::InvalidCertificateHandler> ptrHandler =
+      new Poco::Net::AcceptCertificateHandler(false);
+    Poco::Net::Context::Ptr ptrContext = new Poco::Net::Context(Poco::Net::Context::CLIENT_USE, "");
+    Poco::Net::SSLManager::instance().initializeClient(0, ptrHandler, ptrContext);
     // Creating HTTPS Session
-    HTTPSClientSession client(uri.getHost(), uri.getPort(), ptrContext);
+    Poco::Net::HTTPSClientSession client(uri.getHost(), uri.getPort(), ptrContext);
     std::cout << "Created HTTPS Session" << std::endl;
 
     // Creating Request
-    HTTPRequest *request =
-      new HTTPRequest(
-        HTTPRequest::HTTP_GET,
+    Poco::Net::HTTPRequest *request =
+      new Poco::Net::HTTPRequest(
+        Poco::Net::HTTPRequest::HTTP_GET,
         "/api/v1/osrs/latest/",
-        HTTPRequest::HTTP_1_1
+        Poco::Net::HTTPRequest::HTTP_1_1
       );
     request->add(
       "User-Agent",
@@ -37,14 +34,14 @@ int main() {
     std::cout << "Sent Request" << std::endl;
 
     // Getting Response and saving response header
-    HTTPResponse response;
+    Poco::Net::HTTPResponse response;
     std::istream& rs = client.receiveResponse(response);
     std::cout << "Received Response" << std::endl;
     std::ofstream responseFile("response.txt");
     response.write(responseFile);
     responseFile.close();
 
-  } catch (Exception& e) {
+  } catch (Poco::Exception& e) {
     std::cout << e.displayText() << std::endl;
   }
   std::cout << "Exiting" << std::endl;
