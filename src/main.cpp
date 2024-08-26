d412308be70fd1d85fa1bff6db98962e4e3a87a4
Landon Deam <landondeam@gmail.com>
2024-08-26 19:16:58 -0400

    cleaned up code and added some more debugging

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,50 @@
 #include "./main.h"
 
+using namespace Poco;
+using namespace Net;
+
 int main() {
   try {
-    const Poco::URI uri("https://prices.runescape.wiki");
-    Poco::Net::initializeSSL();
-    Poco::SharedPtr<Poco::Net::InvalidCertificateHandler> ptrHandler = new Poco::Net::AcceptCertificateHandler(false);
-    Poco::Net::Context::Ptr ptrContext = new Poco::Net::Context(Poco::Net::Context::CLIENT_USE, "");
-    Poco::Net::SSLManager::instance().initializeClient(0, ptrHandler, ptrContext);
-
-    Poco::Net::HTTPSClientSession client(uri.getHost(), uri.getPort(), ptrContext);
+    // Initializing variables
+    const URI uri("https://prices.runescape.wiki");
+    initializeSSL();
+    // TODO - Need to make an actual certificate handler :P
+    SharedPtr<InvalidCertificateHandler> ptrHandler =
+      new AcceptCertificateHandler(false);
+    Context::Ptr ptrContext = new Context(Context::CLIENT_USE, "");
+    SSLManager::instance().initializeClient(0, ptrHandler, ptrContext);
+    // Creating HTTPS Session
+    HTTPSClientSession client(uri.getHost(), uri.getPort(), ptrContext);
     std::cout << "Created HTTPS Session" << std::endl;
-    Poco::Net::HTTPRequest *test = new Poco::Net::HTTPRequest(Poco::Net::HTTPRequest::HTTP_GET, "/api/v1/osrs/latest/", Poco::Net::HTTPRequest::HTTP_1_1);
-    test->add("User-Agent", "Landon Deam (landondeam@gmail.com) - Testing a GE tracker");
 
-    std::ostream& sentRequest = client.sendRequest(*test);
+    // Creating Request
+    HTTPRequest *request =
+      new HTTPRequest(
+        HTTPRequest::HTTP_GET,
+        "/api/v1/osrs/latest/",
+        HTTPRequest::HTTP_1_1
+      );
+    request->add(
+      "User-Agent",
+      "Landon Deam (landondeam@gmail.com)\n  Testing a GE tracker"
+    );
+
+    // Sending Request
+    std::ostream& sentRequest = client.sendRequest(*request);
+    std::ofstream requestFile("request.txt");
+    request->write(requestFile);
+    requestFile.close();
     std::cout << "Sent Request" << std::endl;
 
-    Poco::Net::HTTPResponse response;
+    // Getting Response and saving response header
+    HTTPResponse response;
     std::istream& rs = client.receiveResponse(response);
     std::cout << "Received Response" << std::endl;
-    std::ofstream ofs("response.txt");
-    response.write(ofs);
-    std::cout << "Wrote To File" << std::endl;
-    ofs.close();
-  } catch (Poco::Exception& e) {
+    std::ofstream responseFile("response.txt");
+    response.write(responseFile);
+    responseFile.close();
+
+  } catch (Exception& e) {
     std::cout << e.displayText() << std::endl;
   }
   std::cout << "Exiting" << std::endl;
