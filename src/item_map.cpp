7a86d614a2c08e8b61abdfc05a9a652dad330ff6
Landon Deam <landondeam@gmail.com>
2024-09-08 17:01:08 -0400

    changed map to unordered_map and added docs to item_map.cpp

--- src/item_map.cpp
+++ src/item_map.cpp
@@ -2,8 +2,12 @@
 
 #include "item_map.h"
 
-std::map<int, item> item_map::items;
+/// @brief unordered map storing all items' current data
+std::unordered_map<int, item> item_map::items;
 
+/// @brief Uses `data` to update the item map with the given `type`
+/// @param data `std::ostringstream` containing JSON data to parse
+/// @param type `std::string` representing what is in `data`
 void item_map::update(std::ostringstream& data, const std::string& type) {
   if (!type.compare("price")) {
     update_price(data);
@@ -12,16 +16,23 @@ void item_map::update(std::ostringstream& data, const std::string& type) {
   }
 }
 
+/// @brief Gets the `item` at the given `ID` in `item_map`
+/// @param ID Integer representing the in-game ID of the desired item
+/// @return `item` with the given `ID`
 item item_map::get(int ID) {
   return items.at(ID);
 }
 
+/// @brief Ensures that an `item` with `ID` exists by creating it if it doesn't
+/// @param ID ID to verify exists
 void item_map::verifID(int ID) {
   if (items.find(ID) == items.end()) {
     items.emplace(ID, ID);
   }
 }
 
+/// @brief Updates the price of all `item`s
+/// @param data `std::ostringstream` with JSON data to parse into price data
 void item_map::update_price(std::ostringstream& data) {
   std::regex srch(R"#("(\d+)":\{"high":(\d+),"highTime":(\d+),"low":(\d+),"lowTime":(\d+)\})#",
     std::regex_constants::ECMAScript);
@@ -40,6 +51,8 @@ void item_map::update_price(std::ostringstream& data) {
   }
 }
 
+/// @brief Updates all of the mapping data for the items
+/// @param data `std::ostringstream` with JSON data to parse into mapping data
 void item_map::update_info(std::ostringstream& data) {
   std::regex srch(R"#(\{"examine":"(.+?)","id":(\d+),"members":(true|false),"lowalch":(\d+),"limit":(\d+),"value":(\d+),"highalch":(\d+),"icon":"(.+?)","name":"(.+?)"\})#",
     std::regex_constants::ECMAScript);
--- src/item_map.h
+++ src/item_map.h
@@ -4,7 +4,7 @@
 #ifndef ITEM_MAP_H_
 #define ITEM_MAP_H_
 
-#include <map>
+#include <unordered_map>
 #include <string>
 #include <iostream>
 #include <regex>
@@ -15,7 +15,7 @@ class item_map {
   static void update(std::ostringstream& data, const std::string& type);
   static item get(int ID);
  private:
-  static std::map<int, item> items;
+  static std::unordered_map<int, item> items;
   static void verifID(int ID);
   static void update_price(std::ostringstream& data);
   static void update_info(std::ostringstream& data);
