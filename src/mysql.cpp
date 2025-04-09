c31361da3d60d8c7428a5b6d8f65fb2c31ee102a
Landon Deam <landondeam@gmail.com>
2025-04-09 12:12:48 -0400

    add more database connection logic

--- src/mysql.cpp
+++ src/mysql.cpp
@@ -37,3 +37,49 @@ void db_connection::close() {
     std::cerr << e.what() << std::endl;
   }
 }
+
+void db_connection::connect_db() {
+  db =
+    std::make_shared<mysqlx::Schema>(sess->getSchema("osrs_market"));
+
+  if (db->existsInDatabase()) {
+    connect_tables();
+  } else {
+    build_schema();
+  }
+}
+
+void db_connection::connect_tables() {
+  item_map =
+      std::make_shared<mysqlx::Table>(db->getTable("item_map"));
+  if (!item_map->existsInDatabase()) {
+    build_item_map();  // Create item_map table if it doesn't exist
+  }
+
+  price_updated =
+    std::make_shared<mysqlx::Table>(db->getTable("price_updated"));
+  if (!price_updated->existsInDatabase()) {
+    build_price_updated();  // Create price_updated table if it doesn't exist
+  }
+
+  price_series =
+    std::make_shared<mysqlx::Table>(db->getTable("price_series"));
+  if (!price_series->existsInDatabase()) {
+    build_price_series();  // Create price_series table if it doesn't exist
+  }
+}
+
+void db_connection::build_schema() {
+  // Build schema
+  // Create tables
+  connect_tables();
+}
+
+void db_connection::build_price_series() {
+}
+
+void db_connection::build_price_updated() {
+}
+
+void db_connection::build_item_map() {
+}
--- src/mysql.h
+++ src/mysql.h
@@ -15,9 +15,20 @@ class db_connection {
   inline static std::string port;
   inline static std::string host;
   inline static std::shared_ptr<mysqlx::Session> sess;
+  inline static std::shared_ptr<mysqlx::Schema> db;
+  inline static std::shared_ptr<mysqlx::Table> price_series;
+  inline static std::shared_ptr<mysqlx::Table> price_updated;
+  inline static std::shared_ptr<mysqlx::Table> item_map;
+
+  static void connect_tables();
+  static void build_schema();
+  static void build_price_series();
+  static void build_price_updated();
+  static void build_item_map();
 
  public:
   static void init();
+  static void connect_db();
   static void close();
 };
 
