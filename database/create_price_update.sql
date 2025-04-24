3773b6ce666528b507f28c1f187fa6b90ec2ccda
Landon Deam <landondeam@gmail.com>
2025-04-24 00:27:21 -0400

    fixed ordering to reflect updated time for (hopefully) faster r/w

--- database/create_price_update.sql
+++ database/create_price_update.sql
@@ -7,6 +7,6 @@ updated TIMESTAMP(0) NOT NULL,
 
 FOREIGN KEY (ID) REFERENCES osrs_market.item_map(ID) ON UPDATE CASCADE,
 
-PRIMARY KEY (ID, price_type, updated)
+PRIMARY KEY (updated, ID, price_type)
 );
 )""
